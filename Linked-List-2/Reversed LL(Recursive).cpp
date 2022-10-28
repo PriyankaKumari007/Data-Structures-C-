64cd73e69fed5965bc49afe12e9c3f4647eca939
PriyankaKumari007 <89522439+PriyankaKumari007@users.noreply.github.com>
2022-10-28 18:02:25 +0530

    Add files via upload

---
+++ Linked-List-2/Reversed LL(Recursive).cpp
@@ -0,0 +1,44 @@
+/*
+Code : Reverse LL (Recursive)
+
+Given a singly linked list of integers, reverse it using recursion and return the head to the modified list. You have to do this in O(N) time complexity where N is the size of the linked list.
+ Note :
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^4
+Where M is the size of the singly linked list.
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+1 2 3 4 5 6 7 8 -1
+Sample Output 1 :
+8 7 6 5 4 3 2 1
+*/
+
+Node *reverseLinkedListRec(Node *head)
+{
+  if(head==NULL|| head->next==NULL)
+      return head;
+    Node *smallAns=reverseLinkedListRec(head->next);
+    Node *tmp=smallAns;
+    while(tmp->next!=NULL)
+    {
+        tmp=tmp->next;
+    }
+    tmp->next=head;
+    head->next=NULL;
+    return smallAns;
+    
+}
